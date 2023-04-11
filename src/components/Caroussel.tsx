8c82303cbd68b47e91c4a1c228976e09cf05f45c
Nolann Holvoet <nolann.holvoet@epitech.eu>
2023-04-11 04:23:30 -0700

    [FIX] : Fixed arrow comportement on mobile

--- src/components/Caroussel.tsx
+++ src/components/Caroussel.tsx
@@ -28,7 +28,7 @@ const Caroussel = () => {
     clickHandler: React.MouseEventHandler<HTMLDivElement> | any
   ) => (
     <div
-      className={`lg:absolute hidden top-0 bottom-0 ${direction}-0 lg:left-6s flex justify-center items-center  z-20`}
+      className={` hidden md:flex md:absolute md:bottom-0 md:top-0 ${direction}-0  flex justify-center items-center  z-20`}
     >
       <FaArrowCircleLeft
         className={`lg:w-12 lg:h-12 w-8 h-8 text-primal opacity-30  transform hover:opacity-100 cursor-pointer ${

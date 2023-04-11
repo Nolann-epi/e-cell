31dba448f66a0ee4e0e0a60aff6e2f3b01b80e8c
Nolann Holvoet <nolann.holvoet@epitech.eu>
2023-04-10 17:51:11 -0700

    [ADD] : Project Initialisation with Next & tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primal: "#15b7b9",
+        secondary: "#a65dcb",
+      },
+    },
+  },
+  plugins: [],
+};

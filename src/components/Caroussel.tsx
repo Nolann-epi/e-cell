354a295d33ba63d59937f99436441e78e353277a
Nolann Holvoet <nolann.holvoet@epitech.eu>
2023-04-10 17:52:16 -0700

    [ADD] : Carroussel & Carrousel Items

---
+++ src/components/Caroussel.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import { FaArrowCircleLeft } from "react-icons/fa";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import CarousselItem from "./CarousselItem";
+
+const Caroussel = () => {
+  const indicatorStyle = {
+    marginLeft: 20,
+    color: "#15b7b9",
+    cursor: "pointer",
+    height: 12,
+    width: 12,
+    borderRadius: "50%",
+    border: "1px solid #15b7b9",
+    display: "inline-block",
+  };
+
+  const selectedIndicatorStyle = {
+    ...indicatorStyle,
+    color: "white",
+    backgroundColor: "#15b7b9",
+    border: "none",
+  };
+
+  const renderArrow = (
+    direction: string,
+    clickHandler: React.MouseEventHandler<HTMLDivElement> | undefined
+  ) => (
+    <div
+      className={`absolute top-0 bottom-0 ${direction}-0 flex justify-center items-center p-16 opacity-30 hover:opacity-100 cursor-pointer z-20`}
+      onClick={clickHandler}
+    >
+      <FaArrowCircleLeft
+        className={`w-12 h-12 text-primal transform ${
+          direction === "left" ? "" : "rotate-180"
+        }`}
+      />
+    </div>
+  );
+
+  const renderIndicator = (
+    onClickHandler: any,
+    isSelected: boolean,
+    index: number,
+    label: string
+  ) => {
+    const style = isSelected ? selectedIndicatorStyle : indicatorStyle;
+    return (
+      <div
+        style={style}
+        onClick={onClickHandler}
+        onKeyDown={onClickHandler}
+        key={index}
+        role="button"
+        tabIndex={0}
+        aria-label={`${label} ${index + 1}`}
+      />
+    );
+  };
+
+  return (
+    <Carousel
+      autoPlay
+      infiniteLoop
+      showStatus={false}
+      className="h-[calc(100vh-78px)] w-full"
+      renderArrowPrev={(clickHandler) => renderArrow("left", clickHandler)}
+      renderArrowNext={(clickHandler) => renderArrow("right", clickHandler)}
+      renderIndicator={renderIndicator}
+    >
+      <CarousselItem />
+      <CarousselItem />
+      <CarousselItem />
+    </Carousel>
+  );
+};
+
+export default Caroussel;
---
+++ src/components/CarousselItem.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CarousselItem = () => {
+  return (
+    <div className=" h-[calc(100vh-78px)] w-full">
+      <p className="text-primal w-fit">Legend 1</p>
+    </div>
+  );
+};
+
+export default CarousselItem;
